1a6e04c011ae37afa70ec69042161427bb7eed63
lisassssz <lizasmargun@gmail.com>
2024-09-06 16:21:06 +0300

    Список персонажей

--- App.js
+++ App.js
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import axios from "axios";
 
-export default function App() {
+import {
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  Alert,
+  FlatList,
+  ActivityIndicator,
+  View,
+  Text,
+  RefreshControl,
+  TouchableOpacity,
+} from "react-native";
+
+import CharacterCard from "./components/CharacterCard";
+import { useEffect, useState } from "react";
+
+export default function HomeScreen() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [items, setItems] = useState();
+
+  const fetchChatacters = () => {
+    setIsLoading(true);
+    axios
+      .get("https://rickandmortyapi.com/api/character")
+      .then(({ data }) => {
+        setItems(data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+        Alert.alert("Ошибка", "Не удалось получить персонажей");
+      })
+      .finally(() => setIsLoading(false));
+  };
+  useEffect(fetchChatacters, []);
+
+  if (isLoading) {
+    return (
+      <View style={styles.loader}>
+        <ActivityIndicator size="large" />
+        <Text style={styles.loaderText}>Загрузка...</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        refreshControl={
+          <RefreshControl refreshing={isLoading} onRefresh={fetchChatacters} />
+        }
+        data={items}
+        renderItem={({ item }) => (
+          <TouchableOpacity>
+            <CharacterCard
+              name={item.name}
+              status={item.status}
+              species={item.species}
+              location={item.location.name}
+              image={item.image}
+              episode={item.episode.name}
+            />
+          </TouchableOpacity>
+        )}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#2e2e2e",
+    paddingTop: Platform.OS === "android" ? 25 : 0,
+  },
+  loader: {
+    flex: 1,
+    backgroundColor: "#2e2e2e",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  loaderText: {
+    marginTop: 15,
+    color: "white",
   },
 });
---
+++ components/CharacterCard.jsx
@@ -0,0 +1,106 @@
+import { View, Text, StyleSheet, Platform, Image } from "react-native";
+
+export default function CharacterCard({
+  name,
+  status,
+  species,
+  location,
+  image,
+  episode,
+}) {
+  return (
+    <View style={styles.card}>
+      <Image source={{ uri: `${image}` }} style={styles.image} />
+      <View style={styles.textBlock}>
+        <View>
+          <Text style={[styles.mainText, styles.name]}>{name}</Text>
+          <View style={styles.characteristic}>
+            <View
+              style={[
+                status === "Alive"
+                  ? styles.alive
+                  : status === "Dead"
+                  ? styles.dead
+                  : styles.unknown,
+                styles.circle,
+              ]}
+            ></View>
+            <Text style={[styles.mainText, styles.status]}>
+              {status} - {species}
+            </Text>
+          </View>
+        </View>
+        <View>
+          <Text style={styles.extraText}>Last known location:</Text>
+          <Text style={styles.mainText}>{location}</Text>
+        </View>
+        <View>
+          <Text style={styles.extraText}>First seen in:</Text>
+          <Text style={styles.mainText}>{episode}</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    flexDirection: "row",
+    backgroundColor: "#424242",
+    borderRadius: 16,
+    margin: 16,
+    ...Platform.select({
+      ios: {
+        shadowOffset: { width: 2, height: 2 },
+        shadowColor: "333",
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 5,
+      },
+    }),
+  },
+  image: {
+    width: "40%",
+    borderTopLeftRadius: 16,
+    borderBottomLeftRadius: 16,
+  },
+  textBlock: {
+    width: "60%",
+    paddingHorizontal: 12,
+    paddingVertical: 9,
+    gap: 8,
+  },
+  mainText: {
+    color: "#ffffff",
+  },
+  extraText: {
+    color: "#9e9e9e",
+  },
+  name: {
+    fontSize: 20,
+  },
+  status: {
+    fontWeight: "bold",
+  },
+  circle: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+  },
+  alive: {
+    backgroundColor: "#b9f830",
+  },
+  dead: {
+    backgroundColor: "#f84f30",
+  },
+  unknown: {
+    backgroundColor: "#9e9e9e",
+  },
+  characteristic: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 5,
+  },
+});
